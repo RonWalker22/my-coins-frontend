4b7e2868c5c9a3ac7c1036e50bef3443be2e5f6b
Ron Walker <ron22@hey.com>
2022-02-23 07:45:50 -0800

    fix frontend folder structure

--- Bitcoooonnect/configurations/cloudformation/createProjectMacOnly.bat
+++
@@ -1,9 +0,0 @@
-aws cloudformation delete-stack --region us-east-2 --stack-name cryptoservice-createtables
-
-sleep 60
-
-aws cloudformation create-stack --region us-east-2 --stack-name cryptoservice-createtables --template-body file://tables.template.yml --capabilities CAPABILITY_IAM
-
-sleep 10
-
-aws dynamodb batch-write-item --region us-east-2 --request-items file://user1.json
