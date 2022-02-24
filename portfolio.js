f357afd354a2d64f9f7b1a26530e52ba4ea11a37
Ron Walker <ron22@hey.com>
2022-02-24 06:29:27 -0800

    remove getCoinPrices method

--- portfolio.js
+++ portfolio.js
@@ -82,46 +82,6 @@ function getCoins(){
   })
 }
 
-function getCoinPrices(){
-  //Grabs username and passowrd from document to be used as parameters for API
-  let email = COGNITO_ID_TOKEN.email;
-  let BITCOOOONECT_API = `https://t3d210uhn7.execute-api.us-east-2.amazonaws.com/test/coins?${["btc","ada","eth"].map((n, index) => `ids[${index}]=${n}`).join('&')}`;
-
-  const body = {
-    "ids": [
-        "btc",
-        "eth",
-        "woo"
-      ]
-  }
-
-  axios.get(BITCOOOONECT_API, {
-      
-        headers: {
-          'Authorization': COGNITO_AUTH_TOKEN
-      }
-  }).then((res) => {
-    try {
-        objKeys = Object.keys(res.data.user.coins);
-        objValues = Object.values(res.data.user.coins);
-        console.log(res)
-        console.log(res.data)
-        for(let i = 0; i < Object.keys(res.data.user.coins).length; i++) {
-          console.log(objKeys[i]);
-          console.log(objValues[i]);
-          //converts number to be number with 8 decimal places
-          let value = (objValues[i]).toFixed(8); 
-          localStorage.setItem(String(objKeys[i]),String(value));
-        }
-        for (i = 0; i < localStorage.length; i++)   {
-          console.log(localStorage.key(i) + "=[" + localStorage.getItem(localStorage.key(i)) + "]");
-        }
-    } catch (error) { 
-      alert("API offline:COIN API");
-    }
-    
-  })
-}
 //needs tweaking-J
 function createAccount() {
 
